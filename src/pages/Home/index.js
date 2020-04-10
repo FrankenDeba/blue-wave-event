bd6666e6f33b9fb0f23e885bbd7d280c44ce2574
debashis debnath <debashishrambhola@gmail.com>
2020-04-10 20:28:13 +0530

    team 2.0

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ import LOGO from "../../assets/Header/logo.png";
 import Header from "../../Components/Header/Header.js";
 import Footer from "../../Components/Footer/Footer";
 import Work from "../../Components/Work/Work";
-import callUs from "../../Components/callUs/callUs";
+import CallUs from "../../Components/CallUs/CallUs";
 
 
 
@@ -18,7 +18,7 @@ const Home =(props)=>{
          <Navbar/>
         
         <Landing src={LANDING_IMG}/>
-        <callUs/>
+        <CallUs/>
         <Quote line = "Leave it on Blue Waves to make any event truly memorable"/> 
         <Client/>
         <Work/>
